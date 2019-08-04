553739e7c8af5b2f7948e1cf4744df1c6086c6f1
bastien03 <bastienmary@gmail.com>
2019-08-04 23:59:12 +0200

    replace Google Maps with OpenStreetMaps

--- app/app.js
+++ app/app.js
@@ -1,7 +1,12 @@
 let markersArray = [];
 let secondMarkersArray = [];
+let mapInstance;
 
-function initMap() {
+function initApp() {
+  mapInstance = L.map('map');
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+   }).addTo(mapInstance);
   const data = [...jsonData];
   const countries = data.map(countryData => countryData.countryName);
   const selectMap1 = document.getElementById('select_map_1');
@@ -17,74 +22,54 @@ const onCountrySelected = (data, selectCountry, selectMap1, selectMap2) => () =>
   const country = data.filter(d => d.countryName === selectedCountry)[0];
   selectMap1.innerHTML = '<option>--</option>' +
     country.maps.map(m => `<option>${m.mapTitle}</option>`);
-  const map = renderMap(country.mapCenter, country.mapZoom);
-  selectMap1.onchange = onMap1Selected(data, selectedCountry, map, selectMap1, selectMap2);
+  mapInstance.setView([country.mapCenter.lat, country.mapCenter.lng], country.mapZoom);
+  selectMap1.onchange = onMap1Selected(data, selectedCountry, selectMap1, selectMap2);
 };
 
-const renderMap = (center, zoom) => {
-  const map = new google.maps.Map(document.getElementById('map'), {
-    zoom,
-    center: {lat: Number(center.lat), lng: Number(center.lng)}
-  });
-  return map;
-}
-
-const onMap1Selected = (data, selectedCountry, map, selectMap1, selectMap2) => () => {
+const onMap1Selected = (data, selectedCountry, selectMap1, selectMap2) => () => {
   const country = data.filter(d => d.countryName === selectedCountry)[0];
   const selectedMap = selectMap1.value;
   deleteMarkers(secondMarkersArray);
   selectMap2.innerHTML = '<option>--</option>';
   const otherMaps = country.maps.filter(m => m.mapTitle !== selectedMap);
   selectMap2.innerHTML += otherMaps.map(m => `<option>${m.mapTitle}</option>`);
-  selectMap2.onchange = onMap2Selected(data, selectedCountry, map, selectMap2);
+  selectMap2.onchange = onMap2Selected(data, selectedCountry, selectMap2);
   const mapMarkers = country.maps
     .filter(m => m.mapTitle === selectedMap)
     .map(m => m.items)[0];
   deleteMarkers(markersArray);
-  markersArray = renderMarkers(map, mapMarkers);
+  markersArray = renderMarkers(mapMarkers, 'marker_group1');
 };
 
-const onMap2Selected = (data, selectedCountry, map, selectMap2) => () => {
+const onMap2Selected = (data, selectedCountry, selectMap2) => () => {
   const country = data.filter(d => d.countryName === selectedCountry)[0];
   const selectedMap = selectMap2.value;
   const mapMarkers = country.maps
     .filter(m => m.mapTitle === selectedMap)
     .map(m => m.items)[0];
-    const icon = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
   deleteMarkers(secondMarkersArray);
-  secondMarkersArray = renderMarkers(map, mapMarkers, icon);
+  secondMarkersArray = renderMarkers(mapMarkers, 'marker_group2');
 };
 
-const renderMarkers = (map, markers, icon='') => {
-  return markers.map(marker =>
-    new google.maps.Marker({
-      position: {lat: Number(marker.lat), lng: Number(marker.lng)},
-      map: map,
-      title: marker.title,
-      icon
-    })
-  );
-}
-
-// Sets the map on all markers in the array.
-function setMapOnAll(array, map) {
-  for (var i = 0; i < array.length; i++) {
-    array[i].setMap(map);
-  }
-}
-
-// Removes the markers from the map, but keeps them in the array.
-function clearMarkers(array) {
-  setMapOnAll(array, null);
-}
-
-// Shows any markers currently in the array.
-function showMarkers(map) {
-  setMapOnAll(map);
+const renderMarkers = (elements, className) => {
+  const markerIcon = L.divIcon({
+    className
+  });
+  return elements.map(element => {
+    const marker = L.marker([element.lat, element.lng],
+      {
+        icon: markerIcon,
+        title: element.title
+      });
+    marker.addTo(mapInstance);
+    return marker;
+  });
 }
 
 // Deletes all markers in the array by removing references to them.
 function deleteMarkers(array) {
-  clearMarkers(array);
+  for (var i = 0; i < array.length; i++) {
+    array[i].removeFrom(mapInstance);
+  }
   array = [];
 }
