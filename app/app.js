9bdb061c5b76ced1b933aa2dd010be5af7fb168d
bastien03 <bastienmary@gmail.com>
2019-08-04 22:17:41 +0200

    Refactor data structure

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 let markersArray = [];
 let secondMarkersArray = [];
 let mapInstance;
+let footballClubsData;
 
 function initApp() {
   mapInstance = L.map('map');
@@ -8,58 +9,60 @@ function initApp() {
        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
    }).addTo(mapInstance);
   const data = [...jsonData];
-  const countries = data.map(countryData => countryData.countryName);
+  footballClubsData = {...footballClubs};
   const selectMap1 = document.getElementById('select_map_1');
   const selectMap2 = document.getElementById('select_map_2');
 
   const selectCountry = document.getElementById('select_country');
-  selectCountry.innerHTML += countries.map(c => `<option>${c}</option>`);
+  selectCountry.innerHTML += data.map(c => `<option value=${c.countryCode}>${c.countryName}</option>`);
   selectCountry.onchange = onCountrySelected(data, selectCountry, selectMap1, selectMap2);
 };
 
 const onCountrySelected = (data, selectCountry, selectMap1, selectMap2) => () => {
-  const selectedCountry = selectCountry.value;
-  const country = data.filter(d => d.countryName === selectedCountry)[0];
+  const selectedCountryCode = selectCountry.value;
+  const selectedCountry = data.find(d => d.countryCode === selectedCountryCode);
   selectMap1.innerHTML = '<option>--</option>' +
-    country.maps.map(m => `<option>${m.mapTitle}</option>`);
-  mapInstance.setView([country.mapCenter.lat, country.mapCenter.lng], country.mapZoom);
+    selectedCountry.maps.map(m => `<option value=${m.id}>${m.title}</option>`);
+  mapInstance.setView([selectedCountry.mapCenter.lat, selectedCountry.mapCenter.lng], selectedCountry.mapZoom);
   selectMap1.onchange = onMap1Selected(data, selectedCountry, selectMap1, selectMap2);
 };
 
 const onMap1Selected = (data, selectedCountry, selectMap1, selectMap2) => () => {
-  const country = data.filter(d => d.countryName === selectedCountry)[0];
-  const selectedMap = selectMap1.value;
+  const selectedMapId = selectMap1.value;
   deleteMarkers(secondMarkersArray);
   selectMap2.innerHTML = '<option>--</option>';
-  const otherMaps = country.maps.filter(m => m.mapTitle !== selectedMap);
-  selectMap2.innerHTML += otherMaps.map(m => `<option>${m.mapTitle}</option>`);
+  const otherMaps = selectedCountry.maps.filter(m => m.id !== selectedMapId);
+  selectMap2.innerHTML += otherMaps.map(m => `<option value=${m.id}>${m.title}</option>`);
   selectMap2.onchange = onMap2Selected(data, selectedCountry, selectMap2);
-  const mapMarkers = country.maps
-    .filter(m => m.mapTitle === selectedMap)
-    .map(m => m.items)[0];
+  const mapMarkers = getMapItems(selectedCountry, selectedMapId);
   deleteMarkers(markersArray);
   markersArray = renderMarkers(mapMarkers, 'marker_group1');
 };
 
 const onMap2Selected = (data, selectedCountry, selectMap2) => () => {
-  const country = data.filter(d => d.countryName === selectedCountry)[0];
-  const selectedMap = selectMap2.value;
-  const mapMarkers = country.maps
-    .filter(m => m.mapTitle === selectedMap)
-    .map(m => m.items)[0];
+  const selectedMapId = selectMap2.value;
+  const mapMarkers = getMapItems(selectedCountry, selectedMapId);
   deleteMarkers(secondMarkersArray);
   secondMarkersArray = renderMarkers(mapMarkers, 'marker_group2');
 };
 
-const renderMarkers = (elements, className) => {
+const getMapItems = (selectedCountry, selectedMapId) => {
+  const selectedMap = selectedCountry.maps.find(m => m.id === selectedMapId);
+  if (selectedMap.category !== 'FOOTBALL' || selectedCountry.countryCode !== 'fr') {
+    return selectedMap.items;
+  }
+  return selectedMap.items.map(i => footballClubsData[selectedCountry.countryCode][i]);
+}
+
+const renderMarkers = (items, className) => {
   const markerIcon = L.divIcon({
     className
   });
-  return elements.map(element => {
-    const marker = L.marker([element.lat, element.lng],
+  return items.map(item => {
+    const marker = L.marker([item.lat, item.lng],
       {
         icon: markerIcon,
-        title: element.title
+        title: item.title
       });
     marker.addTo(mapInstance);
     return marker;
